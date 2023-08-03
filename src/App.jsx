406032f8c5de1b7e5bcc66f72b13383f1137b5ea
Anthony Nanfito <81890820+ananfito@users.noreply.github.com>
2023-08-03 11:42:38 +0700

    fixed shuffling of answer choices

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Questions from './Questions'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [score, setScore] = useState(0)
   const [gameStatus, setGameStatus] = useState(false)
   const [quizData, setQuizData] = useState([])
 
@@ -48,8 +49,17 @@ function App() {
             questionId: nanoid()
           }
         ))
+
+        console.log("modifiedAPIData:", modifiedAPIData) // REMOVE LATER
+
+        const randomizedAPIData = modifiedAPIData.map(questionObj => {
+          const randomizedAnswersArr = shuffleArray( [...questionObj.answerChoices] )
+          return {...questionObj, answerChoices: randomizedAnswersArr}
+        })
+        console.log("randomized answers:", randomizedAPIData) // REMOVE LATER
+
         // update state
-        setQuizData(modifiedAPIData)
+        setQuizData(randomizedAPIData)
       })
   }, [count]) // do I need a dependency array? Or should I have a function that calls the API? Need to re-address once I'm to the "new game" button
   
@@ -78,12 +88,26 @@ function App() {
     // need to map over the answerChoices array
     // need to select the `isSelected` element and switch data type
     // update state
-    console.log('you clicked something')
-    console.log(id)
-    // setQuizData(prevQuizData => prevQuizData.map((questionObj) => {console.log(questionObj)}))
+
     setQuizData(prevQuizData => prevQuizData.map(questionObj => {
+
       return {...questionObj, answerChoices: questionObj.answerChoices.map(answerChoiceObj => {
+        // need to check if id matches and change isSelected
+        // but also need to check if other IDs are selected and change them to the opposite
+        // check answerChoiceObj.isSelected
+
+        // if the input id matches the answerId, then I want to change isSeleted to be true 
+        // if the input id DOES NOT match the answerId, then I want isSelected to be false
+        // if (answerChoiceObj.answerId === id) {
+        //   return {...answerChoiceObj, isSelected: !answerChoiceObj.answerChoice}
+        // } 
+        // if (answerChoiceObj.answerId === !id) {
+        //   return {...answerChoiceObj, isSelected: false}
+        // } 
+
+        // ORIGINAL
         return answerChoiceObj.answerId === id ? {...answerChoiceObj, isSelected: !answerChoiceObj.isSelected} : answerChoiceObj
+
       })}
     }))
   }
--- src/Questions.jsx
+++ src/Questions.jsx
@@ -4,7 +4,7 @@ import { nanoid, random } from 'nanoid'
 function Questions(props) {
   const { quizData, shuffleArray, selectAnswer } = props
   const styles = {
-    backgroundColor: 'none'
+    backgroundColor: ''
   }
   const selectedChoice = {
     backgroundColor: 'pink'
@@ -12,26 +12,24 @@ function Questions(props) {
   const arr = [0, 1, 2, 3]  
   const quizElements = quizData.map(questionObj => 
     {  
-      const randomizedArr = shuffleArray([...arr])
-
       const questionId = questionObj.questionId
       const questionText = questionObj.question
 
-      const answerId1 = questionObj.answerChoices[randomizedArr[0]].answerId
-      const answerChoice1 = questionObj.answerChoices[randomizedArr[0]].answerChoice
-      const answerChoice1Selected = questionObj.answerChoices[randomizedArr[0]].isSelected
+      const answerId1 = questionObj.answerChoices[0].answerId
+      const answerChoice1 = questionObj.answerChoices[0].answerChoice
+      const answerChoice1Selected = questionObj.answerChoices[0].isSelected
 
-      const answerId2 = questionObj.answerChoices[randomizedArr[1]].answerId
-      const answerChoice2 = questionObj.answerChoices[randomizedArr[1]].answerChoice
-      const answerChoice2Selected = questionObj.answerChoices[randomizedArr[1]].isSelected
+      const answerId2 = questionObj.answerChoices[1].answerId
+      const answerChoice2 = questionObj.answerChoices[1].answerChoice
+      const answerChoice2Selected = questionObj.answerChoices[1].isSelected
 
-      const answerId3 = questionObj.answerChoices[randomizedArr[2]].answerId
-      const answerChoice3 = questionObj.answerChoices[randomizedArr[2]].answerChoice
-      const answerChoice3Selected = questionObj.answerChoices[randomizedArr[2]].isSelected
-      const answerId4 = questionObj.answerChoices[randomizedArr[3]].answerId
+      const answerId3 = questionObj.answerChoices[2].answerId
+      const answerChoice3 = questionObj.answerChoices[2].answerChoice
+      const answerChoice3Selected = questionObj.answerChoices[2].isSelected
+      const answerId4 = questionObj.answerChoices[3].answerId
 
-      const answerChoice4 = questionObj.answerChoices[randomizedArr[3]].answerChoice
-      const answerChoice4Selected = questionObj.answerChoices[randomizedArr[3]].isSelected
+      const answerChoice4 = questionObj.answerChoices[3].answerChoice
+      const answerChoice4Selected = questionObj.answerChoices[3].isSelected
 
       return (
         <div 
